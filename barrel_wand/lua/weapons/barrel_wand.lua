237a7649b83e84121ed3deba811a614fb1444f4a
luke-de-vos <ldevos0@gmail.com>
2022-12-29 15:48:17 -0500

    barrel_wand: refactor, fixed parry, added bot behavior

--- barrel_wand/lua/weapons/barrel_wand.lua
+++ barrel_wand/lua/weapons/barrel_wand.lua
@@ -1,10 +1,12 @@
 AddCSLuaFile()
 print("Executed lua: " .. debug.getinfo(1,'S').source)
 
-resource.AddFile("sound/body_medium_impact_hard6.wav")
-resource.AddFile("sound/body_medium_impact_hard4.wav")
-resource.AddFile("sound/body_medium_impact_hard5.wav")
-
+if SERVER then
+	resource.AddFile("sound/body_medium_impact_hard6.wav")
+	resource.AddFile("sound/body_medium_impact_hard4.wav")
+	resource.AddFile("sound/body_medium_impact_hard5.wav")
+	resource.AddFile("sound/parry_44.wav")
+end
 
 if SERVER then
 	resource.AddFile("materials/VGUI/ttt/icon_barrel_wand.jpg")
@@ -14,7 +16,8 @@ SWEP.Base = "weapon_tttbase"
 
 SWEP.ShootSound = 	Sound("Weapon_Crossbow.BoltFly")
 SWEP.ReloadSound = 	Sound("Weapon_StunStick.Activate")
-SWEP.ParrySound = Sound("")
+SWEP.HotSound = "Weapon_PhysCannon.Launch"
+SWEP.ParrySound = "parry_44.wav"
 
 SWEP.ViewModel			= "models/weapons/c_stunstick.mdl"
 SWEP.WorldModel			= "models/weapons/c_stunstick.mdl"
@@ -58,9 +61,15 @@ SWEP.PropDuration = 5
 SWEP.ThrowForce = 175000
 
 SWEP.IsHot = false
+SWEP.LastParryTime = 0
 SWEP.LastJumpTime = 0
 
+SWEP.PrimaryRof = 0.75
+SWEP.SecondaryRof = 1.0
+SWEP.InterRof = 0.2
+
 local PARRY_WINDOW = 0.2
+local PARRY_THROW_WINDOW = 0.7
 
 local WAND_PROP_PREFIX = "WP"
 local PREFIX_LEN = string.len(WAND_PROP_PREFIX)
@@ -72,74 +81,71 @@ local SMACK_SOUNDS = {
 	"body_medium_impact_hard4.wav",
 	"body_medium_impact_hard6.wav"
 	}
+local CLANK_SOUNDS = {
+	"physics/metal/metal_barrel_impact_soft1.wav",
+	"physics/metal/metal_barrel_impact_soft2.wav",
+	"physics/metal/metal_barrel_impact_soft3.wav",
+	"physics/metal/metal_barrel_impact_soft4.wav"
+}
 local HOT_PROPS = {
+	"models/props_c17/oildrum001.mdl",
 	--"models/props_interiors/Furniture_Couch01a.mdl",
 	--"models/props_c17/FurnitureChair001a.mdl",
 	--"models/props_c17/FurnitureWashingmachine001a.mdl",
 	--"models/props_c17/chair02a.mdl",
 	--"models/props_lab/filecabinet02.mdl",
 	--"models/props_junk/watermelon01.mdl",
-	"models/props_lab/huladoll.mdl",
+	--"models/props_lab/huladoll.mdl",
 	--"models/props_junk/Wheebarrow01a.mdl"
 	}
 
 
-function SWEP:Reload()
-	
-end
-
 function SWEP:PrimaryAttack()
-	if self:GetOwner():GetAmmoCount(self.Primary.Ammo) >= HOT_BARREL_COST then
-		self.IsHot = true
-		self:EmitSound("Weapon_PhysCannon.Launch", 100, 100)
-		self:TakePrimaryAmmo(HOT_BARREL_COST)
-	end
-	self.Weapon:SendWeaponAnim( ACT_VM_MISSCENTER )
-	self:SetNextPrimaryFire(math.max((CurTime() + 0.75), self:GetNextPrimaryFire()))
-	self:SetNextSecondaryFire(math.max((CurTime() + 0.2 ), self:GetNextSecondaryFire()))
-	self:EmitSound(self.ShootSound)
-	self:ThrowProp("models/props_c17/oildrum001.mdl", self.ThrowForce, self.PropDuration, 1.0)
-	self.IsHot = false
-end
+	if self:GetNextPrimaryFire() <= CurTime() then
+		self:SetNextPrimaryFire(math.max((CurTime() + self.PrimaryRof), self:GetNextPrimaryFire()))
+		--self:SetNextSecondaryFire(math.max((CurTime() + self.InterRof), self:GetNextSecondaryFire()))
 
-function SWEP:GetLastJumpTime()
-	return self.LastJumpTime
-end
+		-- animations
+		self:SendWeaponAnim( ACT_VM_MISSCENTER )
+		self:GetOwner():DoAttackEvent()
 
-if SERVER then 
-	_add_hook("EntityTakeDamage", "bw_takedamage", function(target_ent, dmg) 
-		if target_ent:IsPlayer() and dmg:IsDamageType(DMG_CRUSH) then
-			local wep = target_ent:GetActiveWeapon()
-			if IsValid(wep) and wep:GetPrintName() == 'barrel_wand' then
-				if CurTime() - wep:GetLastJumpTime() <= PARRY_WINDOW then
-					print("parried!")
-					dmg:SetDamage(0)
-					wep:GetOwner():Lock()
-					timer.Simple(0.2, function() wep:GetOwner():UnLock() end)
-				end
-			end
+		-- sounds, barrel type
+		if self:GetOwner():GetAmmoCount(self.Primary.Ammo) >= HOT_BARREL_COST then
+			self.IsHot = true
+			self:EmitSound(self.HotSound, 100, 100, 1, CHAN_WEAPON)
+			self:TakePrimaryAmmo(HOT_BARREL_COST)
+		elseif (CurTime() - self:GetLastParryTime()) <= PARRY_THROW_WINDOW then
+			self.IsHot = true
+			self:GetOwner():EmitSound("self.HotSound", 100, 100, 1, CHAN_WEAPON)
+		else 
+			self:EmitSound(self.ShootSound, 100, 100, 1, CHAN_WEAPON)
 		end
-	end)
+		if IsValid(self) then
+			self:ThrowProp("models/props_c17/oildrum001.mdl", self.ThrowForce, self.PropDuration, 1.0)
+			self.IsHot = false
+		end
+	end
 end
 
 function SWEP:SecondaryAttack()
-	self:SetNextPrimaryFire(math.max((CurTime() + 0.2), self:GetNextPrimaryFire()))
-	local next_secondary = math.max((CurTime() + 1.0 ), self:GetNextSecondaryFire())
-	self:SetNextSecondaryFire(next_secondary)
-	self.LastJumpTime = CurTime()
-	self.Weapon:SendWeaponAnim( ACT_VM_MISSCENTER )	
-	if self:GetOwner():KeyDown(4) then -- crouch binding
-		self:GetOwner():SetVelocity(Vector(0,0,500))
-	else
-		self:GetOwner():SetVelocity((self:GetOwner():GetAimVector() + Vector(0,0,0.4)) * 500)
+	--self:SetNextPrimaryFire(math.max((CurTime() + self.InterRof), self:GetNextPrimaryFire()))
+	if self:GetNextSecondaryFire() <= CurTime() then
+		self:SetNextSecondaryFire(math.max((CurTime() + self.SecondaryRof), self:GetNextSecondaryFire()))
+		self.LastJumpTime = CurTime()
+		if self:GetOwner():KeyDown(4) then -- crouch binding
+			self:GetOwner():SetVelocity(Vector(0,0,500))
+		else
+			self:GetOwner():SetVelocity((self:GetOwner():GetAimVector() + Vector(0,0,0.4)) * 500)
+		end
+		-- sound and visual
+		if SERVER then 
+			local effect = EffectData()
+			local origin = self:GetOwner():GetPos()+Vector(0,0,30)
+			_effect("Sparks", origin, 1, 1, 1)
+		end
+		self:GetOwner():DoCustomAnimEvent(PLAYERANIMEVENT_JUMP, 0)
+		self:EmitSound("Grenade.Blip", 50, 100, 1, CHAN_WEAPON)
 	end
-	-- sound and visual
-	if SERVER then 
-		local effect = EffectData()
-		local origin = self:GetOwner():GetPos()+Vector(0,0,30)
-		_effect("Sparks", origin, 1, 1, 1)
-	end
-	self:EmitSound("Grenade.Blip", 50, 100)
 end
 
 function SWEP:ThrowProp(model_file, force_mult, prop_duration, weight_mult)
@@ -160,7 +166,7 @@ function SWEP:ThrowProp(model_file, force_mult, prop_duration, weight_mult)
 	else
 		magic_prop:SetModel(model_file)
 		magic_prop:SetName(MY_BARREL_NAME)
-		magic_prop:SetColor(Color(255,150,150))
+		magic_prop:SetColor(Color(0,255,0))
 	end
 
 	-- aiming, positioning
@@ -178,7 +184,7 @@ function SWEP:ThrowProp(model_file, force_mult, prop_duration, weight_mult)
 	-- spawn
 	magic_prop:Spawn()
 	if self.IsHot then 
-		magic_prop:Ignite(prop_duration, 100) 
+		magic_prop:Ignite(prop_duration, 100)
 	end
 	local phys = magic_prop:GetPhysicsObject()
 	if not IsValid(phys) then magic_prop:Remove() return end
@@ -197,46 +203,190 @@ function SWEP:ThrowProp(model_file, force_mult, prop_duration, weight_mult)
 	end)
 end
 
-function SWEP:PreDrop()
-   return self.BaseClass.PreDrop(self)
+function freeze(func_ply, pos)
+	-- parry freeze frames
+	local hook_name = "BW_freeze_"..func_ply:SteamID()
+	func_ply:SetMoveType(0)
+	hook.Add("Move", hook_name, function(hook_ply, move_info)
+		if hook_ply:Nick() == func_ply:Nick() then
+			move_info:SetVelocity(Vector(0,0,0))
+			move_info:SetOrigin(pos)
+		end
+	end)
+	timer.Simple(0.2, function()
+		hook.Remove("Move", hook_name)
+		func_ply:SetMoveType(2)
+	end)
+end
+
+if SERVER then
+	-- Check for parry and hot barrel
+	hook.Add("EntityTakeDamage", "bw_takedamage", function(target_ent, dmg)
+		if target_ent:IsPlayer() and dmg:GetAttacker():IsPlayer() then
+			local wep = target_ent:GetActiveWeapon()
+			if IsValid(wep) and wep:GetPrintName() == 'barrel_wand' then
+				local diff = CurTime() - wep:GetLastJumpTime() 
+				if diff <= PARRY_WINDOW then -- parried
+					wep:SetLastParryTime(CurTime())
+					wep:SendWeaponAnim( ACT_VM_HITCENTER )
+					freeze(target_ent, target_ent:GetPos())
+					freeze(dmg:GetAttacker(), dmg:GetAttacker():GetPos())
+					dmg:SetDamage(0)
+					wep:EmitSound(wep.ParrySound)
+					dmg:GetAttacker():EmitSound(wep.ReloadSound)
+					if IsValid(dmg:GetInflictor()) and dmg:IsDamageType(DMG_CRUSH) then
+						_effect("Sparks", dmg:GetInflictor():GetPos()+Vector(0,0,20),2,2,2)
+						dmg:GetInflictor():Remove()
+					end
+					-- refresh cooldowns
+					wep:SetNextSecondaryFire(CurTime()+0.2)
+					wep:SetNextPrimaryFire(CurTime()+0.1)
+				else -- got hit	
+					sound.Play(SMACK_SOUNDS[math.random(#SMACK_SOUNDS)], dmg:GetInflictor():GetPos())
+					if dmg:GetInflictor():GetName() == HOT_BARREL_NAME then
+						dmg:SetDamage(600)
+					end
+				end
+			end
+		end
+	end)
 end
 
 function SWEP:AddPhysicsCallback(magic_prop, owner, MY_BARREL_NAME)
 	if magic_prop:GetName() == HOT_BARREL_NAME then
 		magic_prop:AddCallback("PhysicsCollide", function(ent, data)
-			_explosion(owner, data.HitPos, 150, 125) -- radius, damage
+			local hit_ent = data.HitEntity
+			if string.sub(hit_ent:GetName(), 0, PREFIX_LEN) == WAND_PROP_PREFIX then
+				hit_ent:Remove()
+			end
+			-- if data.DeltaTime > 0.5 then
+			-- 	_explosion(owner, data.HitPos, 150, 125) -- radius, damage
+			-- end
 		end)
 	else
 		magic_prop:AddCallback("PhysicsCollide", function(ent, data)
-			-- if collide with prop produced by this player..
 			local hit_ent = data.HitEntity
-			if hit_ent:IsPlayer() then
-				local wep = hit_ent:GetActiveWeapon()
-				if wep:GetPrintName() == self:GetPrintName() then
-					local plus = CurTime() - wep:GetLastJumpTime()
-					--print(plus)
-					if plus <= PARRY_WINDOW then
-						wep:SetNextSecondaryFire(CurTime()+0.1)
-						wep:SetNextPrimaryFire(CurTime()+0.1)
-						wep:EmitSound(self.ReloadSound)
-						if IsValid(magic_prop) then
-							_spark(magic_prop:GetPos())
-							magic_prop:Remove()
-						end
-					end
-				end
-			elseif hit_ent:GetName() == MY_BARREL_NAME || hit_ent:GetName() == HOT_BARREL_NAME then
+			if hit_ent:GetName() == MY_BARREL_NAME || hit_ent:GetName() == HOT_BARREL_NAME then
 				_explosion(owner, data.HitPos, 150, 125) -- radius, damage
 				if IsValid(magic_prop) then magic_prop:Remove() end
 			else
-				if data.OurOldVelocity:Length() >= 700 then -- minimum speed for sparks
-					if hit_ent:IsPlayer() then
-						magic_prop:EmitSound(SMACK_SOUNDS[math.random(#SMACK_SOUNDS)])						
-					elseif string.sub(hit_ent:GetName(), 0, PREFIX_LEN) == WAND_PROP_PREFIX then
+				if data.OurOldVelocity:Length() >= 700 then -- minimum speed for barrel collision effects
+					if string.sub(hit_ent:GetName(), 0, PREFIX_LEN) == WAND_PROP_PREFIX then
+						sound.Play(CLANK_SOUNDS[math.random(#CLANK_SOUNDS)], data.HitPos, 75, 100, 1)
 						_spark(data.HitPos)
 					end
 				end
 			end
 		end)
 	end
+end
+
+function SWEP:Reload()
+	
+end
+function SWEP:PreDrop()
+   return self.BaseClass.PreDrop(self)
+end
+
+function SWEP:GetLastJumpTime()
+	return self.LastJumpTime
+end
+function SWEP:SetLastJumpTime(val)
+	self.LastJumpTime = val
+end
+function SWEP:GetLastParryTime()
+	return self.LastParryTime
+end
+function SWEP:SetLastParryTime(val)
+	self.LastParryTime = val
+end
+
+function get_closest_player(source)
+	if !IsValid(source) then return end
+	local prop_pos = source:GetPos()
+	local best_ply = nil
+	local this_distance = 0
+	local lowest_distance = 9999999
+	for i, ply in ipairs(player.GetAll()) do
+		if source:Nick() == ply:Nick() then continue end
+		if not ply:Alive() then continue end
+		--if ply:GetRole() != ROLE_INNOCENT then continue end
+		local ply_pos = ply:GetPos()
+		ply_pos.z = ply_pos.z + 30 -- center mass
+		this_distance = _get_euc_dist(prop_pos, ply_pos)
+		if this_distance < lowest_distance then
+			lowest_distance = this_distance
+			best_ply = ply
+		end
+	end
+	return best_ply
+end
+
+-- if SERVER then
+-- Entity(1):SetObserverMode(6)
+-- end
+
+function attempt_attack(ply, attack_type)
+	-- attack_type (int) 1 or 2 for primary and secondary attack respectively
+	if IsValid(ply) and IsValid(ply:GetActiveWeapon()) then
+		if attack_type == 1 then
+			ply:GetActiveWeapon():PrimaryAttack()
+		elseif attack_type == 2 then
+			ply:GetActiveWeapon():SecondaryAttack()
+		end			
+	end
+end	
+
+if SERVER then
+	-- bot behavior
+	hook.Add("Think", "BW_bot_behavior", function()
+		for i,ply in ipairs(player.GetAll()) do
+			if ply:SteamID() != "BOT" then continue end
+			local target = get_closest_player(ply)
+			if target != nil then
+				if IsValid(ply) then
+					local r = math.random()
+					if r <= 0.01 then -- do leap
+						ply:SetEyeAngles((target:GetPos() + Vector(0,0,500) - ply:GetPos()):Angle())
+						attempt_attack(ply, 2)
+					elseif r <= 0.15 then -- attack
+						local posdiff = target:GetPos() - ply:GetPos() + VectorRand(-20,20)
+						ply:SetEyeAngles(posdiff:Angle())
+						local tr = ply:GetEyeTrace()
+						if tr.Entity:IsPlayer() then
+							attempt_attack(ply, 1)
+							if math.random() < 0.5 then -- 50% chance for bot target to attempt parry
+								if target:SteamID() == "BOT" then attempt_attack(target, 2) end
+							end
+						elseif _get_euc_dist(tr.HitPos, ply:GetPos()) < 10 then -- likely face against wall
+							ply:GetActiveWeapon():SecondaryAttack()
+						end
+					end
+				end
+			end
+		end
+		
+
+		-- if IsValid(Entity(3)) and Entity(3):IsPlayer() and IsValid(Entity(3):GetActiveWeapon()) then
+		-- 	local r = math.random()
+		-- 	if r <= 0.01 then
+		-- 		if CurTime() > Entity(3):GetActiveWeapon():GetNextSecondaryFire() then
+		-- 			Entity(3):SetEyeAngles((Entity(2):GetPos() + Vector(0,0,500) - Entity(3):GetPos()):Angle())
+		-- 			Entity(3):GetActiveWeapon():SecondaryAttack()
+		-- 		end
+		-- 	elseif r <= 0.15 then
+		-- 		if CurTime() > Entity(3):GetActiveWeapon():GetNextPrimaryFire() then
+		-- 			local posdiff = Entity(2):GetPos() - Entity(3):GetPos() + VectorRand(-30,30)
+		-- 			Entity(3):SetEyeAngles(posdiff:Angle())
+		-- 			if Entity(3):GetEyeTrace().Entity:IsPlayer() then
+		-- 				Entity(3):GetActiveWeapon():PrimaryAttack()
+		-- 				if math.random() < 0.5 then 
+		-- 					Entity(2):GetActiveWeapon():SecondaryAttack()
+		-- 				end
+		-- 			end
+		-- 		end
+		-- 	end
+		-- end
+	end)
+	--hook.Remove("Think", "BW_bot_behavior")
 end
