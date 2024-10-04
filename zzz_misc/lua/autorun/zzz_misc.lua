6fe6816155ae3226e8d0c1b541d2c590d0551dd5
luke-de-vos <ldevos0@gmail.com>
2024-10-04 14:24:30 -0400

    misc: prevent dropping crowbar and magneto stick on death

--- zzz_misc/lua/autorun/zzz_misc.lua
+++ zzz_misc/lua/autorun/zzz_misc.lua
@@ -168,6 +168,15 @@ if SERVER then
 end
 
 
+-- on DoPlayerDeath, remove magneto stick and crowbar from player
+if SERVER then
+	hook.Add("DoPlayerDeath", "RemoveMagnetoStickAndCrowbar", function(victim, attacker, dmginfo)
+		victim:StripWeapon("weapon_zm_improvised")
+		victim:StripWeapon("weapon_zm_carry")
+	end)
+end
+
+
 
 
 /*
